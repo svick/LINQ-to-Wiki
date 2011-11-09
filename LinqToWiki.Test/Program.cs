c75bd8fafa037934fffe4a502b4684b8cdb6f251
Petr Onderka <gsvick@gmail.com>
2011-11-09 20:50:51 +0100

    empty project

---
+++ LinqToWiki.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqToWiki.Test
+{
+    static class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
