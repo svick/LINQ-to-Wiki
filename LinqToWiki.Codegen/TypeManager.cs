3cd8cfffd83ffbc9e696c36a370371d5f3969226
Petr Onderka <gsvick@gmail.com>
2015-07-04 17:51:04 +0200

    volatile is a keyword

--- LinqToWiki.Codegen/TypeManager.cs
+++ LinqToWiki.Codegen/TypeManager.cs
@@ -204,7 +204,7 @@ namespace LinqToWiki.Codegen
         /// <summary>
         /// Names of enum members that are keywords and have to be prefixed by a <c>@</c>.
         /// </summary>
-        private static readonly string[] Restricted = new[] { "new", "true", "false", "lock" };
+        private static readonly string[] Restricted = new[] { "new", "true", "false", "lock", "volatile" };
 
         /// <summary>
         /// Fixes the name of an enum member, so that it's a valid C# identifier.
